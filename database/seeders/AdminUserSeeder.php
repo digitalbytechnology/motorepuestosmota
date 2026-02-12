67449d4212c18d557a3f98e0fbed176694fc0a51
Hugoda <hhugodiaz23@gmail.com>
2026-02-11 21:07:07 -0600

    Arreglo de seeder admin

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -5,17 +5,29 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class AdminUserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::updateOrCreate(
-            ['email' => 'admin@gmail.com'],
+        // Limpiar cache de permisos
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Asegurar que el rol exista
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+
+        // Crear o actualizar usuario
+        $user = User::updateOrCreate(
+            ['email' => 'hhugodiaz23@gmail.com'],
             [
                 'name' => 'Administrador',
                 'password' => Hash::make('hugo123.'),
             ]
         );
+
+        // Asignar rol admin
+        $user->syncRoles([$adminRole]);
     }
 }
